acd0f1cb250de0c59b333a575314233eb5493ff4
Tae Hyeon Jo <hillskuti@naver.com>
2022-06-18 11:18:45 +0900

    Fetching 컴포넌트 추가

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,17 @@
+import { useFetch } from '../../hooks/useFetch';
+
 // style
 import './Home.css';
-
 import React from 'react';
 
 export default function Home() {
-  return <div>Home</div>;
+  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+
+  return (
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>로딩중...</p>}
+      {data && data.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
