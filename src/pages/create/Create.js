a08db805a89c6a0832ea79e73edadc210bcf964d
Tae Hyeon Jo <hillskuti@naver.com>
2022-06-19 14:26:16 +0900

    레시피 만들기 컴포넌트 생성

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -2,7 +2,54 @@
 import './Create.css';
 
 import React from 'react';
+import { useState } from 'react';
 
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>요리 제목</span>
+          <input
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>요리 방법</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          ></textarea>
+        </label>
+
+        <label>
+          <span>요리시간:</span>
+          <input
+            type='text'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className='btn'>제출</button>
+      </form>
+    </div>
+  );
 }
