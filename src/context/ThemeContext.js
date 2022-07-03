30c37c8d30b8e3ceaf9068b69698e9684b86a641
Tae Hyeon Jo <hillskuti@naver.com>
2022-07-03 18:24:25 +0900

    Add Context Api Comment

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,5 +1,7 @@
+// This function allows us to create a new context object.
 import { createContext } from 'react';
 
+// context object를 ThemeContext으로 export 한다.
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
