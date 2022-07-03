37c6089e848d5cea1f26e4b1c1c0bf6841d7eb9f
Tae Hyeon Jo <hillskuti@naver.com>
2022-07-03 19:10:34 +0900

    Context 객체 값을 일반 컴포넌트에서 사용하는 법

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+//? Context API를 사용하기 위해 useContext를 넣어줘야함.
 import { useContext } from 'react';
+//? 접근하고 싶은 Context를 가져옴.
 import { ThemeContext } from '../context/ThemeContext';
-import Searchbar from './Searchbar';
 
+// 컴포넌트
+import Searchbar from './Searchbar';
+// 스타일
 import './Navbar.css';
 
 export default function Navbar() {
+  //? useContext에 내가 접근하고 싶은 context object 이름을 입력해주면 된다.
+  //? 여기에서는 ThemeContext Object의 color 값을 디스트럭쳐링한다.
+  //! 주의 : function명이 아니라 Object 명을 입력해야한다.
   const { color } = useContext(ThemeContext);
 
   return (
+    // 인라인 스타일로 컬러를 넣어줌.
     <div className='navbar' style={{ background: color }}>
       <nav>
         <Link to='/' className='brand'>
